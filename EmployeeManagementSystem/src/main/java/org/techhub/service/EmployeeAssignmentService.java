882ab5baffbd09e7e8d96d07b59eef1f5e32b58c
vidyapatil09 <130054193+vidyapatil09@users.noreply.github.com>
2024-12-26 18:06:42 +0530

    Revert "Merge branch 'master' of https://github.com/vrushali085/EMS"
    This reverts commit 4378fe9f417fbb08e38e12c504c735de8d6da0f5, reversing
    changes made to c46f0ce1a642184409009cb61875d3cede546d31.

--- EmployeeManagementSystem/src/main/java/org/techhub/service/EmployeeAssignmentService.java
+++ EmployeeManagementSystem/src/main/java/org/techhub/service/EmployeeAssignmentService.java
@@ -3,21 +3,21 @@ package org.techhub.service;
 import java.util.List;
 
 public interface EmployeeAssignmentService {
-	 boolean assignRoleToEmployee(int employeeId, int roleid);
+	 boolean assignRoleToEmployee(int employeeId, int roleId);
 
 	    // Method to get the list of roles assigned to an employee
-	 public List<String> getRolesForEmployee(String employeeName);
+	    List<Integer> getRolesForEmployee(int employeeId);
 
 	    // Method to remove a specific role from an employee
-	 public boolean removeRoleFromEmployee(String employeeName, String roleName);
+	    boolean removeRoleFromEmployee(int employeeId, int roleId);
 
 	    // Method to assign a department to an employee
-	 public boolean assignDepartmentToEmployeeByName(String employeeName, int departmentId);
+	    boolean assignDepartmentToEmployee(int employeeId, int departmentId);
 
 	    // Method to get the list of departments assigned to an employee
-	 public List<String> getDepartmentsForEmployee(int employeeId);
-//
-//	    // Method to remove a specific department from an employee
-	 public boolean removeDepartmentFromEmployee(String employeeName, String departmentName);
+	    List<Integer> getDepartmentsForEmployee(int employeeId);
+
+	    // Method to remove a specific department from an employee
+	    boolean removeDepartmentFromEmployee(int employeeId, int departmentId);
 
 }
--- EmployeeManagementSystem/src/main/java/org/techhub/service/EmployeeAssignmentServiceImpl.java
+++ EmployeeManagementSystem/src/main/java/org/techhub/service/EmployeeAssignmentServiceImpl.java
@@ -1,58 +1,44 @@
 package org.techhub.service;
 
 import java.util.List;
-import org.techhub.repository.EmployeeAssignmentRepository;
-import org.techhub.repository.EmployeeAssignmentRepositoryImpl;
 
 public class EmployeeAssignmentServiceImpl implements EmployeeAssignmentService {
 
-    EmployeeAssignmentRepository repository = new EmployeeAssignmentRepositoryImpl();
-
-    // Assign role by employeeId and role_id
-    @Override
-    public boolean assignRoleToEmployee(int employeeId, int roleId) {
-        return repository.assignRoleToEmployee(employeeId, roleId);
-    }
-
-    
-
-    // Get roles for an employee by their name
-    @Override
-    public List<String> getRolesForEmployee(String employeeName) {
-        return repository.getRolesForEmployee(employeeName);
-    }
-
-    // Assign department to employee by name
-    @Override
-    public boolean assignDepartmentToEmployeeByName(String employeeName, int departmentId) {
-        return repository.assignDepartmentToEmployeeByName(employeeName, departmentId);
-    }
-
-
-
 	@Override
-	public boolean removeRoleFromEmployee(String employeeName, String roleName) {
+	public boolean assignRoleToEmployee(int employeeId, int roleId) {
 		// TODO Auto-generated method stub
-		return repository.removeDepartmentFromEmployee(employeeName, roleName);
+		return false;
 	}
 
-
-
 	@Override
-	public List<String> getDepartmentsForEmployee(int employeeId) {
+	public List<Integer> getRolesForEmployee(int employeeId) {
 		// TODO Auto-generated method stub
-		return repository.getDepartmentsForEmployee(employeeId);
+		return null;
 	}
 
-
-
 	@Override
-	public boolean removeDepartmentFromEmployee(String employeeName, String departmentName) {
+	public boolean removeRoleFromEmployee(int employeeId, int roleId) {
 		// TODO Auto-generated method stub
-		return repository.removeDepartmentFromEmployee(employeeName, departmentName);
+		return false;
 	}
 
+	@Override
+	public boolean assignDepartmentToEmployee(int employeeId, int departmentId) {
+		// TODO Auto-generated method stub
+		return false;
+	}
 
+	@Override
+	public List<Integer> getDepartmentsForEmployee(int employeeId) {
+		// TODO Auto-generated method stub
+		return null;
+	}
 
+	@Override
+	public boolean removeDepartmentFromEmployee(int employeeId, int departmentId) {
+		// TODO Auto-generated method stub
+		return false;
+	}
 	
+
 }
